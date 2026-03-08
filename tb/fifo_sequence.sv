9e4cbaa629a7427a365598611e4dfd466a86ccfb
Shrutipriya <shrutipriya.1735@gmail.com>
2026-03-08 23:58:30 +0530

    Random FIFO stimulus sequence

---
+++ tb/fifo_sequence.sv
@@ -0,0 +1,23 @@
+class fifo_sequence extends uvm_sequence #(fifo_seq_item);
+
+`uvm_object_utils(fifo_sequence)
+
+function new(string name="fifo_sequence");
+   super.new(name);
+endfunction
+
+task body();
+
+fifo_seq_item req;
+
+repeat(20)
+begin
+   req = fifo_seq_item::type_id::create("req");
+   start_item(req);
+   req.randomize();
+   finish_item(req);
+end
+
+endtask
+
+endclass
