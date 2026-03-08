5ce0d377bd46adc2ed88609c9a8bbca8b061181b
Shrutipriya <shrutipriya.1735@gmail.com>
2026-03-08 23:45:35 +0530

    UVM sequence item for FIFO transaction

---
+++ tb/fifo_seq_item.sv
@@ -0,0 +1,13 @@
+class fifo_seq_item extends uvm_sequence_item;
+
+rand bit wr_en;
+rand bit rd_en;
+rand bit [7:0] data;
+
+`uvm_object_utils(fifo_seq_item)
+
+function new(string name="fifo_seq_item");
+   super.new(name);
+endfunction
+
+endclass
